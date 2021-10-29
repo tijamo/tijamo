d0ba3c3ac0804228db10b9ccdb4d986f84d8d06a
Stackbit Code Editor <projects@stackbit.com>
2021-10-29 15:52:16 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -46,7 +46,7 @@ export default class Body extends React.Component {
                       <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                     ) : ((style === 'classic') ? (
                       (font === 'serif') ? (
-                      <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+                      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                       ) : 
                       <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                     ) : 
