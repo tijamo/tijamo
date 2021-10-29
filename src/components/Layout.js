57fe5e5bd866480bde3f8f3e435a2e1c7f8f43ab
Stackbit Code Editor <projects@stackbit.com>
2021-10-29 15:54:13 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -53,7 +53,7 @@ export default class Body extends React.Component {
                       (font === 'serif') ? (
                       <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                       ) : 
-                      <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+                      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                     )}
                     {_.get(this.props, 'pageContext.site.siteMetadata.favicon', null) && (
                     <link rel="icon" href={withPrefix(_.get(this.props, 'pageContext.site.siteMetadata.favicon', null))}/>
