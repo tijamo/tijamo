997dbf5405e89c5edd4785e0caa67d49f06f3fb2
Stackbit Code Editor <projects@stackbit.com>
2021-11-05 20:59:14 +0000

    page.js: changed

--- src/templates/page.js
+++ src/templates/page.js
@@ -25,6 +25,7 @@ export default class Page extends React.Component {
             			<h1 className="page__title">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
             		</header>
             		<div className="page__body text-block">
+                        
             			{htmlToReact(_.get(this.props, 'pageContext.html', null))}
             		</div>
             	</div>
